625d50b171746454330c9a7f3e4e097f6a897bcf
Herbert <herbertlzx@gmail.com>
2023-04-10 18:18:21 +0800

    feat: logo of git

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 // import { css } from "@emotion/react";
-import { GithubOutlined, DatabaseOutlined } from "@ant-design/icons";
+import { DatabaseOutlined } from "@ant-design/icons";
 import { Space, Tooltip, Button, Popover } from "antd";
 // import { flex } from "../../utils/layout";
 import Help from "../Help";
 import { IconFont } from "../IconFont";
 import { openNewTab } from "../../utils/utils";
+import git from "../../static/git.png";
+import { flex } from "../../utils/layout";
 const RightContent = () => {
   return (
     <>
@@ -30,7 +32,9 @@ const RightContent = () => {
         </Tooltip>
         <Tooltip placement="bottom" title="代码库">
           <Button
-            icon={<GithubOutlined />}
+            css={flex}
+            style={{ alignSelf: "center", justifySelf: "center" }}
+            icon={<img src={git} alt="" css={{ width: 22, height: 22 }} />}
             type="text"
             onClick={() => openNewTab("https://git.aiursoft.cn")}
           />
